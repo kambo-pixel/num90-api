976334911a9e649ee77ddbb1935d28125d4ccd92
kambo-pixel <kamboparfait01@gmail.com>
2026-03-14 15:42:13 +0000

    Server.js

---
+++ Server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("NUM90 API fonctionne");
+});
+
+app.get("/resultats", (req, res) => {
+  res.json({
+    tirage: [12, 25, 33, 48, 67]
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Serveur lancé sur le port " + PORT);
+});
