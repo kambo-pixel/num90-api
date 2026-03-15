9d46302fc4c5b90574655932ed60bd488305f2fc
kambo-pixel <kamboparfait01@gmail.com>
2026-03-15 17:16:24 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import axios from "axios";
+import cheerio from "cheerio";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,10 +9,39 @@ app.get("/", (req, res) => {
   res.send("NUM90 API fonctionne");
 });
 
-app.get("/resultats", (req, res) => {
-  res.json({
-    tirage: [12, 25, 33, 48, 67]
-  });
+app.get("/resultats", async (req, res) => {
+  try {
+
+    const url = "https://lotobonheur.ci/resultats";
+
+    const response = await axios.get(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0"
+      }
+    });
+
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    let numeros = [];
+
+    $(".ball, .number, .result").each((i, el) => {
+      const num = $(el).text().trim();
+      if (num) numeros.push(num);
+    });
+
+    res.json({
+      source: url,
+      tirage: numeros
+    });
+
+  } catch (error) {
+
+    res.json({
+      erreur: "Impossible de récupérer les résultats"
+    });
+
+  }
 });
 
 app.listen(PORT, () => {
